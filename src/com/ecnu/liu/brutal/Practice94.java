378b1d0b08689f5b387642fa2f1448a405cd78a4
liuzhi07 <liuzhi07@meituan.com>
2020-02-16 13:58:37 +0800

    94. 二叉树的中序遍历

---
+++ src/com/ecnu/liu/brutal/Practice94.java
@@ -0,0 +1,47 @@
+package com.ecnu.liu.brutal;
+
+import com.ecnu.liu.struct.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: create by liuxiansen
+ * @description: com.ecnu.liu.brutal
+ * @date: Created in 2020-02-16
+ * @modified By:
+ * 给定一个二叉树，返回它的中序 遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,3,2]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Practice94 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        middleVisit(root, result);
+        return result;
+    }
+
+    private void middleVisit(TreeNode node, List<Integer> result){
+        if (node == null) {
+            return;
+        }
+        middleVisit(node.left, result);
+        result.add(node.val);
+        middleVisit(node.right, result);
+    }
+}
