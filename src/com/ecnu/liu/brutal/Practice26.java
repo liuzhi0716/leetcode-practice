f0c97c422ab27cc36b097fe9baeecda784b8eaf2
liuzhi07 <liuzhi07@meituan.com>
2020-02-21 22:08:48 +0800

    26. 删除排序数组中的重复项

---
+++ src/com/ecnu/liu/brutal/Practice26.java
@@ -0,0 +1,32 @@
+package com.ecnu.liu.brutal;
+
+/**
+ * @author: create by liuxiansen
+ * @description: com.ecnu.liu.brutal
+ * @date: Created in 2020-02-21
+ * @modified By:
+ */
+public class Practice26 {
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int l = 0;
+        int space = 0;
+        //比较以后相等将每次的space拉开，因为l是从最初往后的，所以会自动吧相等的数的位置填上
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[l] == nums[i]) {
+                space++;
+            } else {
+                nums[i-space] = nums[i];
+                l++;
+            }
+        }
+        return nums.length  - space;
+    }
+
+    public static void main (String[] args) {
+        removeDuplicates(new int[]{1,1,2});
+    }
+}
