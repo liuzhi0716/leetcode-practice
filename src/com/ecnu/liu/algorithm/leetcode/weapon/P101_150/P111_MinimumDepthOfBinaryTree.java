2432f1984d0431abeddbdcc565b276151eaa6bb3
Xiaobin Liu <xiaobing.liu@cootek.cn>
2020-02-26 12:01:27 +0800

    111. 二叉树的最小深度

--- src/com/ecnu/liu/algorithm/leetcode/weapon/P101_150/P111_MinimumDepthOfBinaryTree.java
+++ src/com/ecnu/liu/algorithm/leetcode/weapon/P101_150/P111_MinimumDepthOfBinaryTree.java
@@ -38,6 +38,7 @@ public class P111_MinimumDepthOfBinaryTree {
         if (root == null) return 0;
         if (root.left == null) return minDepth(root.right) + 1;
         if (root.right == null) return minDepth(root.left) + 1;
+
         return Math.min(minDepth(root.left)+1, minDepth(root.right)+1);
     }
 }
