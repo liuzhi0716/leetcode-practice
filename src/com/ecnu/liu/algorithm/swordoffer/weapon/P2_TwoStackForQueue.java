e4503bf692aba1fa1e17927e55df4ef14de6a1c5
Xiaobin Liu <xiaobing.liu@cootek.cn>
2020-02-29 11:11:56 +0800

    2. 用两个栈实现队列

--- src/com/ecnu/liu/algorithm/swordoffer/weapon/P2_TwoStackForQueue.java
+++ src/com/ecnu/liu/algorithm/swordoffer/weapon/P2_TwoStackForQueue.java
@@ -4,6 +4,7 @@ import java.util.Stack;
 
 /**
  * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * https://www.nowcoder.com/practice/54275ddae22f475981afa2244dd448c6?tpId=13&tqId=11158&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
  */
 public class P2_TwoStackForQueue {
 
