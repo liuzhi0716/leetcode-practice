890d671736fe8106dbb000cb5d4803498a6f1358
Xiaobin Liu <xiaobing.liu@cootek.cn>
2020-02-29 11:13:10 +0800

    5. 矩形覆盖

--- src/com/ecnu/liu/algorithm/swordoffer/weapon/P5_RectCover.java
+++ src/com/ecnu/liu/algorithm/swordoffer/weapon/P5_RectCover.java
@@ -20,6 +20,7 @@ public class P5_RectCover {
         if (target == 1) return 1;
         else if (target == 2) return 2;
         else return RectCover(target-1) + RectCover(target-2);
+
     }
 
 }
