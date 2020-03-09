ee1396b3917f3fa252b42bb378b0fb6cc59b9be3
liuzhi07 <liuzhi07@meituan.com>
2020-03-09 09:49:20 +0800

    ok

---
+++ src/com/ecnu/liu/algorithm/concurrent/Practice0.java
@@ -0,0 +1,45 @@
+package com.ecnu.liu.algorithm.concurrent;
+
+/**
+ * @author: create by liuxiansen
+ * @description: com.ecnu.liu.algorithm.concurrent
+ * @date: Created in 2020-03-02
+ * @modified By:
+ */
+public class Practice0 {
+
+
+    public static void main (String[] args) {
+        Integer i = 5;
+        Thread a = new Thread(() -> {
+            synchronized (i) {
+                for (int j = 0; j < 26; j++) {
+                    int c = 'a' + j;
+                    System.out.println((char) c);
+                    i.notify();
+                    try {
+                        i.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        Thread b = new Thread(() -> {
+            synchronized (i) {
+                for (int j = 0; j < 26; j++) {
+                    System.out.println(j);
+                    i.notify();
+                    try {
+                        i.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        a.start();
+        b.start();
+    }
+
+}
---
+++ src/com/ecnu/liu/algorithm/swordoffer/brutal/Practice7.java
@@ -0,0 +1,21 @@
+package com.ecnu.liu.algorithm.swordoffer.brutal;
+
+import com.ecnu.liu.algorithm.struct.TreeNode;
+
+/**
+ * @author: create by liuxiansen
+ * @description: com.ecnu.liu.algorithm.swordoffer.brutal
+ * @date: Created in 2020-02-29
+ * @modified By:
+ * 题目描述
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ */
+public class Practice7 {
+
+//    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+//        if (root2 == null || root1 == null) {
+//            return false;
+//        }
+//        if(root1.val != root2.val)
+//    }
+}
