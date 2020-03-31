02f24e3a966140d56f6431b5401c25db375b5ce7
liuzhi07 <liuzhi07@meituan.com>
2020-03-31 10:44:36 +0800

    31

--- src/com/ecnu/liu/algorithm/leetcode/brutal/fifty/Practice31.java
+++ src/com/ecnu/liu/algorithm/leetcode/brutal/fifty/Practice31.java
@@ -23,6 +23,20 @@ package com.ecnu.liu.algorithm.leetcode.brutal.fifty;
  */
 public class Practice31 {
 
+    /**
+     *
+     * 从最右边开始，设最右边的数是nums[i]，那么每次从i的右边寻找，看能不能找到一个数比i大，如果有，就交换它们，并把i右边的数由小到大排序，如果找不到，就i--，继续下一次寻找
+     *
+     * 为什么这样做呢？
+     *
+     * 因为我想找一个比当前数字大的数，并且这个数需要在某个位上比当前数字大，那么我们只需要找到某个位于高位的数，这个数比低位上的某个数要小，交换它们，就可以使我们找到的数比当前数字大了。
+     *
+     * 又因为我们要找的数需要是最接近当前数字的数，所以我们需要从右边每个位开始找，判断是否它的低位（即右边）是否有比它大的数，有我们就交换它们，此外这样还不够接近，还需要把低位上的所有数排序一遍，才能是最接近的数
+     * 思路参考：https://blog.csdn.net/weixin_40673608/article/details/83998859
+     * 代码参考：https://blog.csdn.net/qq_41231926/article/details/82491091
+     *
+     * @param nums
+     */
     public void nextPermutation(int[] nums) {
 
     }
